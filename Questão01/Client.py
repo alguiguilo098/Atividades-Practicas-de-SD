36de2f5286b8639d0779c0ec1c8157da19976cf2
alguiguilo098 <guiguigui098@gmail.com>
2025-04-25 21:09:43 -0400

    feat: CREATEUSER

--- Questão01/Client.py
+++ Questão01/Client.py
@@ -1,1 +1,25 @@
 import hashlib
+import sys
+from SocketCliente  import SocketClient
+
+if __name__=="__main__":
+    print("Initializing Client...")
+    socketclient=SocketClient()
+    socketclient.connect('localhost', int(sys.argv[1]))
+    socket=socketclient.socket
+    print("Connected to server")
+    path:str="/"
+    while True:
+        print(f"$[{path}]:",end="")
+        command=input().split(" ")
+        if command[0]=="EXIT":
+            break
+        elif command[0]=="CONNECT":
+            pass
+        elif command[0]=="CREATEUSER":
+            print("Username:",end="")
+            username=input()
+            print("Password:",end="")
+            password=input()
+            passha256=hashlib.sha256(password.encode()).hexdigest()
+            socket.send(f"CREATEUSER {username} {passha256}".encode())
--- Questão01/Server.py
+++ Questão01/Server.py
@@ -1,14 +1,34 @@
 from User import *
+import sys
+import hashlib
+from SocketServerOwn import ServerSocket
 import os 
 
 if __name__=="__main__":
 
+    pathfilesys="../FileSystem/"
+
+    db.connect()
     db.create_tables([User])
 
-    uncle_bob = User(name='Bob', password="1234")
-    uncle_bob.save() 
+    user=None
+    
+    print("Initializing Server...")
 
-    if os.path.exists('users.db'):
-        os.remove('users.db')
-        print("Database deleted")
-        db.close()
+    socketserver=ServerSocket('localhost', int(sys.argv[1]),int(sys.argv[2]))
+    (conn,host)=socketserver.accept()
+
+    print("Server started")
+
+    while True:
+        data=conn.recv(1024).decode().split(" ")
+        if data[0]=="EXIT":
+            conn.send(f"User {data[1]} disconnected".encode())
+        if data[0]=="CONNECT":
+            pass
+        elif data[0]=="CREATEUSER":
+            passsha256=hashlib.sha256(data[1].encode()).hexdigest()
+            user=User(name=data[1],password=passsha256)
+            user.save()
+            conn.send("User created")
+        
