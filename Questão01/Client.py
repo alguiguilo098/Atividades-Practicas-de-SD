d44cd93ae036bebac592302e66802cd728ce42bc
alguiguilo098 <guiguigui098@gmail.com>
2025-04-26 00:14:01 -0400

    feat: pwd implementation

--- Questão01/Client.py
+++ Questão01/Client.py
@@ -1,21 +1,31 @@
 import hashlib
 import sys
+import socket as sc
 from SocketCliente  import SocketClient
 
 if __name__=="__main__":
     print("Initializing Client...")
     socketclient=SocketClient()
-    socketclient.connect('localhost', int(sys.argv[1]))
+    hostname:str=sc.gethostbyname(sc.gethostname())
+    socketclient.connect(hostname, int(sys.argv[1]))
     socket=socketclient.socket
     print("Connected to server")
     path:str="/"
     while True:
         print(f"$[{path}]:",end="")
         command=input().split(" ")
+        
         if command[0]=="EXIT":
             break
         elif command[0]=="CONNECT":
-            pass
+            if len(command)<=2:
+                print("Invalid command")
+                continue
+            passha256=hashlib.sha256(command[2].encode()).hexdigest()
+            socket.send(f"CONNECT {command[1]} {passha256}".encode())
+            print("Connecting...")
+            print(socket.recv(1024).decode())
+
         elif command[0]=="CREATEUSER":
             print("Username:",end="")
             username=input()
@@ -23,3 +33,9 @@ if __name__=="__main__":
             password=input()
             passha256=hashlib.sha256(password.encode()).hexdigest()
             socket.send(f"CREATEUSER {username} {passha256}".encode())
+            print(socket.recv(1024).decode())
+        elif command[0]=="PWD":
+            socket.send("PWD".encode())
+            print(socket.recv(1024).decode())
+        else:
+            print("Command not found")
--- Questão01/Server.py
+++ Questão01/Server.py
@@ -1,34 +1,63 @@
 from User import *
 import sys
 import hashlib
+import socket as sc
 from SocketServerOwn import ServerSocket
 import os 
 
+def connect(conn, data):
+    try:
+        user=User.get(User.name==data[1]) # busca do usuario no banco de dados
+
+        if user.password==data[2] and user.name==data[1]:
+            conn.send("SUCCESS".encode())
+            print(f"User {user.name} connected")
+        else:
+            conn.send('ERROR'.encode())
+            print(f"User {user.name} not connected")
+    except User.DoesNotExist:
+        print("User not found")
+        conn.send("ERROR".encode())
+
+def create_user(conn, data):
+    passsha256=hashlib.sha256(data[1].encode()).hexdigest()
+    User.create(name=data[1], password=passsha256)
+    print(f"User created {data[1]}")
+    conn.send("User created".encode())
+
 if __name__=="__main__":
 
-    pathfilesys="../FileSystem/"
+    pathfilesys=["../FileSystem/","/"]
 
     db.connect()
     db.create_tables([User])
-
-    user=None
     
-    print("Initializing Server...")
+    user=None
 
-    socketserver=ServerSocket('localhost', int(sys.argv[1]),int(sys.argv[2]))
+    print("Initializing Server...")
+    
+    hostname:str=sc.gethostbyname(sc.gethostname())
+    print(f"Hostname: {hostname}")
+
+    socketserver:ServerSocket=ServerSocket(hostname, int(sys.argv[1]),int(sys.argv[2]))
     (conn,host)=socketserver.accept()
 
     print("Server started")
 
     while True:
-        data=conn.recv(1024).decode().split(" ")
+        data=conn.recv(1024).decode()
+        if data!="PWD":
+            print("Não pwd")
+            data=data.split(" ")
+
+        print(f"Received data: {data}")
         if data[0]=="EXIT":
-            conn.send(f"User {data[1]} disconnected".encode())
-        if data[0]=="CONNECT":
-            pass
+            print(f"User {data[1]} disconnected".encode())
+        elif data[0]=="CONNECT":
+            connect(conn, data)
         elif data[0]=="CREATEUSER":
-            passsha256=hashlib.sha256(data[1].encode()).hexdigest()
-            user=User(name=data[1],password=passsha256)
-            user.save()
-            conn.send("User created")
+            create_user(conn, data)
+        elif data=="PWD":
+            pwd=pathfilesys[1::]
+            conn.send(f"Current path: {pwd}".encode())
         
