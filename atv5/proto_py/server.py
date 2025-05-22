bb7616a985f81859014b38991b81bada9cbd0027
alguiguilo098 <guiguigui098@gmail.com>
2025-05-22 13:06:22 -0400

    feat: finish work

--- atv5/proto_py/server.py
+++ atv5/proto_py/server.py
@@ -80,6 +80,7 @@ class MovieServer:
                 print("OK")
         except Exception as e:
             print(e)
+            
         
 
     def get_filmes(self,cliente, atores=[], generos=[]):
